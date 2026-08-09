40e1501e3a7874ec3bd966dd6227c8217b411695
souvikjs01 <souvik741156@gmail.com>
2026-08-09 22:50:43 +0530

    added better auth

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,10 +1,32 @@
 import express from "express";
 import dotenv from "dotenv";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
+import cors from "cors";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 8001;
 const app = express();
+app.use(
+    cors({
+        origin: true,
+        credentials: true,
+    })
+);
+
+// IMPORTANT: Better Auth BEFORE express.json()
+app.all("/api/auth/*splat", toNodeHandler(auth));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello from http backend");
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
-})
+});
+
+// POST /api/auth/sign-up/email
+// POST /api/auth/sign-in/email
+// POST /api/auth/sign-out
--- apps/backend/src/lib/auth.ts
+++ apps/backend/src/lib/auth.ts
@@ -1,13 +1,18 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "@twello/db/client";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const auth = betterAuth({
+    baseURL: process.env.BETTER_AUTH_URL!,
     database: prismaAdapter(prisma, {
         provider: "postgresql",
     }),
-
     emailAndPassword: {
         enabled: true,
     },
+    trustedOrigins: [
+        "http://localhost:3000",
+    ],
 });
